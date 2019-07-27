e1eacd4822c570deed375fcdd8a87038c28f194a
leelavathikm1 <leelavathikm1@gmail.com>
2019-07-27 18:49:34 +0530

    built in methods

--- JS/CRUD-APP2/main.js
+++ JS/CRUD-APP2/main.js
@@ -1,5 +1,4 @@
 function getData() {
-  console.log("getData is Called");
   var obj = {
     name: document.getElementById("name").value,
     email: document.getElementById("email").value,
@@ -17,7 +16,7 @@ function sendData(obj) {
   sendInfo.onreadystatechange = function() {
     if (sendInfo.readyState == 4 && sendInfo.status == 201) {
       console.log("user added");
-      getlatest();
+      window.location = "allUsers.html";
     }
   };
   sendInfo.open("POST", "http://localhost:3000/users");
@@ -25,13 +24,59 @@ function sendData(obj) {
   sendInfo.send(JSON.stringify(obj));
 }
 
-function getlatest() {
+function getUser() {
+  let obj = JSON.parse(localStorage.getItem("obj"));
+  if (obj != null) {
+    if (obj.isEdit) {
+      getDataFromServer(obj);
+    }
+  }
+}
+
+getUser();
+
+function getDataFromServer(obj) {
   var getInfo = new XMLHttpRequest();
   getInfo.onreadystatechange = function() {
     if (getInfo.readyState == 4 && getInfo.status == 200) {
-      console.log(getInfo.response);
+      var data = JSON.parse(getInfo.response);
+      console.log(data[obj.index]);
+      displayinForm(data[obj.index]);
     }
   };
   getInfo.open("GET", "http://localhost:3000/users");
   getInfo.send();
+}
+
+function displayinForm(obj) {
+  for (a in obj) {
+    if (a != "id") {
+      document.getElementById(a).value = obj[a];
+    }
+  }
+}
+
+function updateUser() {
+  var updatedObj = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    userName: document.getElementById("userName").value,
+    password: document.getElementById("password").value,
+    company: document.getElementById("company").value,
+    country: document.getElementById("country").value,
+    time: document.getElementById("time").value
+  };
+  let obj = JSON.parse(localStorage.getItem("obj"));
+  let ind = obj.index + 1;
+  var getInfo = new XMLHttpRequest();
+  getInfo.onreadystatechange = function() {
+    if (getInfo.readyState == 4 && getInfo.status == 200) {
+      var data = JSON.parse(getInfo.response);
+      console.log(data);
+      window.location = "allUsers.html";
+    }
+  };
+  getInfo.open("PUT", "http://localhost:3000/users/" + ind);
+  getInfo.setRequestHeader("Content-type", "application/json");
+  getInfo.send(JSON.stringify(updatedObj));
 }
