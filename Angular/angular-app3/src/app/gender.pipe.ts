db6f0a9ce5d6b6902a322cd71537ba0d8ca5d521
leelavathikm1 <leelavathikm1@gmail.com>
2019-08-15 16:11:04 +0530

    Adding Custom Pipe

--- Angular/angular-app3/src/app/gender.pipe.ts
+++ Angular/angular-app3/src/app/gender.pipe.ts
@@ -5,11 +5,11 @@ import { Pipe, PipeTransform } from "@angular/core";
 })
 export class GenderPipe implements PipeTransform {
   transform(value: any, ...args: any[]): any {
-    console.log(value);
+    console.log(args);
     if (value == "Male") {
-      return "Mr";
+      return "Mr." + args[0].fname;
     } else {
-      return "Mrs";
+      return "Mrs. " + args[0].fname;
     }
   }
 }
